96191647c8b9556d14eac7805425911f59dcf738
Chipp <mysysteme123@gmail.com>
2025-12-22 17:40:12 +1100

    Push current site state

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,37 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Copy assets through
   eleventyConfig.addPassthroughCopy("my-site/src/assets");
-  
+  eleventyConfig.addPassthroughCopy("admin");
+
   // Watch for changes in CSS and images
   eleventyConfig.addWatchTarget("my-site/src/assets/css/");
   eleventyConfig.addWatchTarget("my-site/src/assets/images/");
-  
+
   // Create blog posts collection
-  eleventyConfig.addCollection("posts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("my-site/src/pages/posts/*.md").reverse();
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("my-site/src/pages/posts/*.md")
+      .reverse();
   });
-  
+
   // Add date filter for post dates
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return new Date(dateObj).toLocaleDateString('en-US', { 
-      year: 'numeric', 
-      month: 'long', 
-      day: 'numeric' 
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return new Date(dateObj).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     });
   });
-  
+
   return {
     dir: {
       input: "my-site/src",
       output: "my-site/dist",
       includes: "components",
       data: "data",
-      layouts: "_layouts"
+      layouts: "_layouts",
     },
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    htmlTemplateEngine: "njk",
   };
 };
