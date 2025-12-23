5af1fe2c12978143fd65ce40adf601f9ef8b5d6c
Chipp <mysysteme123@gmail.com>
2025-12-23 12:03:43 +1100

    Re-push current site state

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,19 @@
 module.exports = function (eleventyConfig) {
-  // Copy assets through (adjusted paths since input is now root)
+  // Copy assets through
   eleventyConfig.addPassthroughCopy("my-site/src/assets");
 
-  // Watch targets (adjusted)
+  // Watch targets
   eleventyConfig.addWatchTarget("my-site/src/assets/css/");
   eleventyConfig.addWatchTarget("my-site/src/assets/images/");
 
-  // Blog posts collection (adjusted glob)
+  // Blog posts collection
   eleventyConfig.addCollection("posts", function (collectionApi) {
     return collectionApi
       .getFilteredByGlob("my-site/src/pages/posts/*.md")
       .reverse();
   });
 
-  // Date filter stays the same
+  // Date filter
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return new Date(dateObj).toLocaleDateString("en-US", {
       year: "numeric",
@@ -24,13 +24,13 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: ".",                         // Root of project
-      output: "_site",
-      includes: "my-site/src/components",
-      data: "my-site/src/data",
-      layouts: "my-site/src/_layouts",
+      input: "my-site/src",
+      output: "my-site/dist",
+      includes: "components",
+      data: "data",
+      layouts: "_layouts"
     },
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
   };
 };
